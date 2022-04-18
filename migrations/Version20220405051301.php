86ec175e6ec1831e9fad810077413dc7b1a2e132
Chasmyr <lestratgabin5886@gmail.com>
2022-04-18 14:38:41 +0200

    Update Version20220405051301.php

--- migrations/Version20220405051301.php
+++ migrations/Version20220405051301.php
@@ -20,7 +20,7 @@ final class Version20220405051301 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD roles');
+        $this->addSql('ALTER TABLE user ADD roles JSON NULL');
     }
 
     public function down(Schema $schema): void
