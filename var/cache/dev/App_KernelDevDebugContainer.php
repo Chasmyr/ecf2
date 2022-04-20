728ac92a2d29073a658d6610dc7471e4676113d7
Chasmyr <lestratgabin5886@gmail.com>
2022-04-20 02:44:29 +0200

    css fix

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \Container5wcbAJF\App_KernelDevDebugContainer([
     'container.build_hash' => '5wcbAJF',
-    'container.build_id' => 'f95a4a6f',
-    'container.build_time' => 1650413398,
+    'container.build_id' => '5b97017d',
+    'container.build_time' => 1650414631,
 ], __DIR__.\DIRECTORY_SEPARATOR.'Container5wcbAJF');
